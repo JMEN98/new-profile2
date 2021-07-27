f0d372ca8e0076321ae6c221f78f26f426219d59
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2021-07-27 12:53:04 -0400

    add to contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,9 @@ function Contact (props){
   return (
   <div  className="contact">
     <h1> jeffbenjamin@gmail.com</h1> 
+    <h1>jeffbenjamin@yahoo.com</h1>
+    <h1>https://www.linkedin.com/in/jeff-benjamin-194263214/ </h1>
+    <h1> https://my.indeed.com/p/jeffb-1poy65f</h1>
   </div>
   
   )
