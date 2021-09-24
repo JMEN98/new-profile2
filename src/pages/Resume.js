69e4ef39a0fda44733684a6abb9284fefc916cc9
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2021-09-24 09:52:46 -0400

    add

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,46 +1,35 @@
 import React from "react";
-import test from "../test.json"
-import skills from"../skills.json"
-  import hire from"../images/hire.jpg"
-
+import test from "../test.json";
+import skills from "../skills.json";
+import hire from "../images/hire.jpg";
+// console.log(skills)
 function Resume(props) {
- console.log(test)
- return(
   
-
-  <div> <h1>behold my Resume </h1>
-  <div className="class"  >
-   <article class="shadow">
-  <h1>{skills.header}</h1> 
-  <h1>{skills.education}</h1>
-  <h5>{skills.h5}</h5>
-  <h1>{skills.job}</h1>
-  <p> {skills.li}</p>
-  <p>{skills.li1}</p>
-  <h1>{skills.h1}</h1>
-  <ol>{skills.skillList}</ol>
-   <img  className="hire" src={hire} width="200px" height="200px" align="center"></img>
-  </article>
-
+  return (
+    <div>
+      <h1>behold my Resume </h1>
+      <div className="class">
+        <article class="shadow">
       
-    
-  </div>
-
-</div>
-
-
-
- 
-    
-
-
-   
-
-
- )
-
-
-
-
+           <h1> {skills.name} </h1>
+           <h1> {skills.h2} </h1>
+           <p> {skills.p} </p>
+            <h1> {skills.job} </h1>
+            <p> {skills["Job history1"]} </p>
+            <p> {skills["Job history2"]} </p>
+            <h1> {skills.s} </h1>
+            <p> {skills.S1} </p>
+            <p> {skills.s2} </p>
+          <img
+            className="hire"
+            src={hire}
+            width="200px"
+            height="200px"
+            align="center"
+          ></img>
+        </article>
+      </div>
+    </div>
+  );
 }
 export default Resume;
