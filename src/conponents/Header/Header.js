553de9501b1c281b1aaa1086558657d6724962f9
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2022-01-09 23:29:25 -0500

    allmost-done

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,6 +1,6 @@
 import react from "react";
 import './header.css'
-
+import
 
 
 function Header() {
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,10 +4,14 @@ import React  from "react";
 function Contact (props){
   return (
   <div  className="contact">
+    <article>
     <h1> jeffbenjamin605@gmail.com</h1> 
     <h1>jeffbenjamin605@yahoo.com</h1>
     <h2>  <a href="https://www.linkedin.com/in/jeff-benjamin-194263214">my-linkedin</a> </h2>
     <h2>  <a href="https://my.indeed.com/p/jeffb-1poy65f">  my-indeed</a></h2>
+
+    </article>
+    
   </div>
   
   )
